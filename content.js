e8e4771db610075f20d0e36839a76daa152953ef
Olivia Liu <86632849+ojiaxil@users.noreply.github.com>
2025-08-12 16:25:41 -0500

    Add hairless cat images

--- content.js
+++ content.js
@@ -1,6 +1,9 @@
-// list of hairless cat images
 const hairlessCatImages = [
-    
+    "images/hairless_cat_1.png",
+    "images/hairless_cat_2.png",
+    "images/hairless_cat_3.png",
+    "images/hairless_cat_4.png",
+    "images/hairless_cat_5.png"
 ];
 
 function getRandomHairlessCats() {
